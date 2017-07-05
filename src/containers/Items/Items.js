2bd55979511b4a19767344e32178e222caa57b8f
Sonia <soniasophiaw@gmail.com>
2017-07-05 15:41:42 -0700

    Initial commit

---
+++ src/containers/Items/Items.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './styles.css';
+
+const Items = () => (
+    <h1>So many items</h1>
+);
+
+export default Items;
---
+++ src/containers/Items/ItemsContainer.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Items from './Items';
+
+class ItemsContainer extends Component {
+    render() {
+        return (
+            <Items />
+        );
+    }
+}
+
+export default ItemsContainer;
